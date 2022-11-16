1b56334d7d35c4450cdd8bcd4afca9d2a8b8750d
Davide Bellini <bellini.davide@gmail.com>
2022-11-16 08:37:47 +0100

    Aggiornate dipendenze

--- dist/cookie-banner.js
+++ dist/cookie-banner.js
@@ -17,18 +17,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var cookieconsent__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! cookieconsent */ "./node_modules/cookieconsent/build/cookieconsent.min.js");
 /* harmony import */ var cookieconsent__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(cookieconsent__WEBPACK_IMPORTED_MODULE_1__);
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-
 
 
 var CookieBanner = /*#__PURE__*/function () {
   function CookieBanner() {
     _classCallCheck(this, CookieBanner);
-
     this._defaultLocale = 'en';
     this._options = {
       locale: document.documentElement.lang || this._defaultLocale,
@@ -44,7 +39,6 @@ var CookieBanner = /*#__PURE__*/function () {
     };
     this._options.cookieConsent = Object.assign(this.events(), this._options.cookieConsent);
   }
-
   _createClass(CookieBanner, [{
     key: "setup",
     value: function setup() {
@@ -158,10 +152,8 @@ var CookieBanner = /*#__PURE__*/function () {
       return contents[locale] || contents[this._defaultLocale];
     }
   }]);
-
   return CookieBanner;
 }();
-
 
 window.CookieBanner = new CookieBanner();
 
