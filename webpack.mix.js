7508a93fc2e5281e16db2c0e55198dc34f9fdee3
Davide Bellini <bellini.davide@gmail.com>
2026-06-12 11:11:32 +0200

    Aggiornamento dipendenze

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,6 @@ const mix = require('laravel-mix')
 
 mix
     .js('src/cookie-banner.js', 'dist/cookie-banner.js')
-    .sass('src/cookie-banner.scss', 'dist/cookie-banner.css');
+    .sass('src/cookie-banner.scss', 'dist/cookie-banner.css', {
+        api: 'modern'
+    });
